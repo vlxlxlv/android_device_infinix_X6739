8fdc60fe944ed36ce74724e14fc8061a32897b6e
Mashopy <eliasgheeraert@gmail.com>
2026-07-23 17:34:46 +0800

    X6739: Use pre-U QPR2 WiFi HAL struct layout
    Change-Id: Ie4287a066ee1fc0c1f9ac4fdedd0aec7c5e8ba2e
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: vlxlxlv <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -435,6 +435,7 @@ PRODUCT_PACKAGES += \
     libutils-v31 
 
 # Wi-Fi
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
 PRODUCT_PACKAGES += \
     libkeystore-wifi-hidl \
     libkeystore-engine-wifi-hidl \
